eae3c1dd3fa230e98b452a50b032219323a20410
c-fergo <71800867+c-fergo@users.noreply.github.com>
2021-09-27 16:42:26 +1000

    Update world.py
    Put in current version of python script

--- python/world.py
+++ python/world.py
@@ -1,1 +1,28 @@
-print("Hello, world!")
+import os
+import time
+
+count = 4 #count is decremented for testing purposes, in practice will be an infinite loop until program is stopped
+while(count>0):
+  print("process ID and connection type")
+  os.system("sudo netstat -p -at")
+    
+  print("kernel routing table")
+  os.system("sudo netstat -r")
+  
+  print("ports used by a process")
+  os.system("sudo netstat -anp")
+  
+  print("network interfaces")
+  os.system("sudo netstat -i")
+  
+  print("show only established connections")
+  os.system(ss | grep -i")
+            
+  print("show who is connected")
+  os.system("who")
+            
+            
+  
+  print("waiting for 10 sec...............................")
+  time.sleep(5)
+  count = count - 1 
