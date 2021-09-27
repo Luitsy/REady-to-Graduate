07e1d0793a763e7bea82cd6bdc911cd04e4060d2
c-fergo <71800867+c-fergo@users.noreply.github.com>
2021-09-27 21:43:48 +1000

    Update world.py

--- python/world.py
+++ python/world.py
@@ -1,30 +1,30 @@
 import os
 import time
 
-print("testing")
+#print("testing")
 
 count = 4 #count is decremented for testing purposes, in practice will be an infinite loop until program is stopped
 while(count>0):
-  print("process ID and connection type")
+  #print("process ID and connection type")
   os.system("sudo netstat -p -at")
     
-  print("kernel routing table")
+ # print("kernel routing table")
   os.system("sudo netstat -r")
   
-  print("ports used by a process")
+  #print("ports used by a process")
   os.system("sudo netstat -anp")
   
-  print("network interfaces")
+ # print("network interfaces")
   os.system("sudo netstat -i")
   
-  print("show only established connections")
-  os.system("ss | grep -i")
+ # print("show only established connections")
+  os.system("ss | grep -i tcp")
             
-  print("show who is connected")
+ # print("show who is connected")
   os.system("who")
             
             
   
-  print("waiting for 10 sec...............................")
+  print("wait...............................")
   time.sleep(5)
   count = count - 1 
