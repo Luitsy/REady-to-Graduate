8411a33481f6aa82090341b0f95ddae5f4f23e21
Luitsy <79773906+Luitsy@users.noreply.github.com>
2021-10-03 21:36:59 +1100

    Update Init_Strawberry.py

--- Init_Strawberry.py
+++ Init_Strawberry.py
@@ -16,3 +16,22 @@ os.system("echo $host_name | sudo tee /etc/hostname")
 os.system("sudo hostnamectl set-hostname strawberryPi")
 os.system("sudo systemctl restart avahi-daemon")
 os.system("sudo su -l pi")
+
+## update the running version of python to python 3
+os.system("update-alternatives --install /usr/bin/python python /usr/bin/python3 1")
+
+## update pip installer
+os.system("pip install --upgrade pip")
+
+## install docker compose - this will take a while
+os.system("sudo pip install docker-compose")
+
+##To pull MISP Container
+os.system("git clone https://github.com/MISP/misp-docker ~/Desktop/MISP")
+os.system("cd misp-docker")
+# Copy template.env to .env (on the root directory) and edit the environment variables at .env file
+os.system("cp template.env .env")
+os.system("vi .env")
+
+
+
