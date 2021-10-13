b2d24b4e7b4a65d0aaaaeab4ec404a5402cd16f6
c-fergo <71800867+c-fergo@users.noreply.github.com>
2021-10-13 14:37:03 +1100

    Update container.py

--- python/container.py
+++ python/container.py
@@ -4,11 +4,11 @@ import time
 count = 1
 while(count > 0): #never ending loop
     print("TESTING TESTING TESTING")
-    os.system("python script.py > data.csv") #runs for 4 hours
+    os.system("python script.py > data.csv") #runs for 4 hours, saves data into data.csv
     #need to convert file into MISP format here
     
     #pushes to github
     os.system("git pull")
     os.system("git add data.csv")
-    os.system(git commit -a -m 'data.csv")
+    os.system("git commit -a -m 'data.csv")
     os.system("git push")
